5eefcb59cd595a99a34751be3862e747517f9066
vincentlie06 <72127176+vincentlie06@users.noreply.github.com>
2021-02-12 13:53:23 +0700

    Update HamPath+Animasi.cpp

--- C++/HamPath+Animasi.cpp
+++ C++/HamPath+Animasi.cpp
@@ -53,25 +53,25 @@ void find_ham(int node,vector<vector<vector<int>>> updated_con,int node_count,ve
     dfs(node,updated_con,storage,node_count,ham_paths);
 }
 
-float getDist(vector<int>x, vector<int> y){
-    int xDist=x[1]-y[1], yDist=x[0]-y[0];
+float getDist(vector<float>x, vector<float> y){
+    float xDist=x[1]-y[1], yDist=x[0]-y[0];
     float Dist=sqrt(xDist*xDist+yDist*yDist);
     return Dist;
     }
 
-float getAngle(vector<int> p1, vector<int> p2){
+float getAngle(vector<float> p1, vector<float> p2){
     float a=p2[1]-p1[1], b=p2[0]-p1[0];
     float c=atan2(a,b);
     float pi=2*acos(0.0);
     return 360-(c*180/pi);
 }
 
-int zoom(vector<vector<int>> nodes){
-    vector<int> x=nodes[0], y=nodes[1];
-    return int(250/getDist(x,y));
+float zoom(vector<vector<float>> nodes){
+    vector<float> x=nodes[0], y=nodes[1];
+    return (250/getDist(x,y));
     }
 int main(){
-    vector<vector<int>> nodes={{0,0},{2,2},{2,0},{1,2}};
+    vector<vector<float>> nodes={{0,0},{2,2},{2,0},{1,2}};
     vector<vector<int>> connections={{0,1},{1,2},{1,3},{2,3}};
     vector<vector<int>> ham_paths;
     int node_count=nodes.size();
@@ -87,10 +87,10 @@ int main(){
     for(vector<int> ham_path: ham_paths){
         print_ham(ham_path);
     }
-    vector<vector<int>> zoomed_nodes;
-    int magnitude = zoom(nodes);
+    vector<vector<float>> zoomed_nodes;
+    float magnitude = zoom(nodes);
     int WHeight=1000, WWidth=1000;
-    for (vector<int> x: nodes){
+    for (vector<float> x: nodes){
         zoomed_nodes.push_back({x[0]*magnitude,x[1]*magnitude});
     }
 
@@ -111,14 +111,14 @@ int main(){
             };
             window.draw(line, 2, sf::Lines);
         }
-        for (vector<int> node : zoomed_nodes){
+        for (vector<float> node : zoomed_nodes){
             sf::CircleShape circle(10.f);
             circle.setPosition(Hcentre+node[0]-10,Wcentre-node[1]-10);
             window.draw(circle);
         }
         vector<int> path=ham_paths[0];
         for (int i=0; i<node_count-1; i++){
-            vector<int> a=zoomed_nodes[path[i]], b=zoomed_nodes[path[i+1]];
+            vector<float> a=zoomed_nodes[path[i]], b=zoomed_nodes[path[i+1]];
             float dist=getDist(a,b), angle=getAngle(a,b);
             sf::RectangleShape line(sf::Vector2f(dist, 5.f));
             line.rotate(angle);
