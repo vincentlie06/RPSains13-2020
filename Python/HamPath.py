b63140eea5bd63519a228722087e336717f807da
vincentlie06 <72127176+vincentlie06@users.noreply.github.com>
2021-07-26 11:11:36 +0700

    rewrite everything
    improve graph building and storing, improve DFS with external visited. merge 2 functions with help of default value.

--- Python/HamPath.py
+++ Python/HamPath.py
@@ -1,33 +1,33 @@
 from time import time
 start=time()
-nodes=[[0, 0], [2, 2], [2, 0], [1, 2]]
 connections=[[0, 1], [1, 2], [1, 3], [2, 3]]
-node_count=len(nodes)
-def find(node):
-    arr=[]
-    for x in connections:
-        if x[0]==node: arr.append(x)
-        elif x[1]==node: arr.append([node,x[0]])
-    return arr
-def search(node,arr):
-    for a in range(len(con[node])):
-        x=con[node][a][1]
-        if x not in arr:
-            arr.append(x)
-            search(x,arr)
-            del arr[-1]
-    if len(arr)==node_count:
-        ham_paths.append(str(arr))
-        print("ham paths:",arr)
-con=[find(x) for x in range(node_count)]
-def ham(node):
-    arr=[]
-    arr.append(node)
-    search(node,arr)
-ham_paths=[]
-for x in range(node_count):
-    ham(x)
+node_count = 4
+def buildGraph(connections, node_count):
+    graph = {i : [] for i in range(node_count)}
+    for x, y in connections:
+        graph[x].append(y)
+        graph[y].append(x)
+    return graph
+def search(node, graph, path = [], visited = dict()):
+    if path == []: path = [node]
+    if visited == dict():
+        visited = {n : False for n in graph}
+        visited[node] = True
+    for v in graph[node]:
+        if not visited[v]:
+            visited[v] = True
+            path.append(v)
+            search(v, graph, path, visited)
+            del path[-1]
+            visited[v] = False
+    if len(path) == len(graph):
+        all_paths.append(path.copy())
+graph = buildGraph(connections, node_count)
+all_paths = []
+for node in range(node_count):
+    search(node, graph)
+for path in all_paths:
+    print(f"{path = }")
 end=time()
-print("Execution time for finding hamiltonian paths: ")
-print(str(end-start)+" seconds")
-    
+print("Execution time for finding paths: ")
+print(f"{end-start} seconds")
